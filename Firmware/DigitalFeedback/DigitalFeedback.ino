5926ef5a3379f87ba43ed18b11099c0cb19fc8f0
Alan-JH <alanjhsu08@gmail.com>
2022-03-12 00:45:34 -0500

    Update DigitalFeedback.ino

--- Firmware/DigitalFeedback/DigitalFeedback.ino
+++ Firmware/DigitalFeedback/DigitalFeedback.ino
@@ -4,16 +4,29 @@
 // PORTA definitions
 #define CS 4
 #define LDAC 5
-#define VIN 6
-#define IIN 7
 
+// ADC definitions
+#define VIN 2
+#define IIN 3
+
+int v;
+int i;
+
+float scalev;
+float scalei;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(0x45);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.setDataMode(SPI_MODE0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  v = analogRead(VIN);
+  i = analogRead(IIN); //EACH ANALOG READ TAKES 100us!!!
 
+  
 }
