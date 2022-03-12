ed40e2bd06bf4a935508d8bde849f2168f742b84
Alan-JH <alanjhsu08@gmail.com>
2022-03-12 16:10:29 -0500

    dac spi

--- Firmware/DigitalFeedback/DigitalFeedback.ino
+++ Firmware/DigitalFeedback/DigitalFeedback.ino
@@ -15,18 +15,28 @@ int i;
 float scalev;
 float scalei;
 
+int dacout;
+
+ADC0.SAMPCTRL = 2; // Decrease number of ADC clocks that the micro waits for. Defaults to 14
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(0x45);
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV4);
   SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   v = analogRead(VIN);
-  i = analogRead(IIN); //EACH ANALOG READ TAKES 100us!!!
+  i = analogRead(IIN); 
+  
+  dacout = (dacout & 0x3ff) << 2;
+  dacout |= (0b0001 << 12);
 
   
+  SPI.transfer(dacout >> 8);
+  SPI.transfer(dacout & 0xff);
 }
