a2f6b3191467da7564fab73330899a734b25bdf7
Alan-JH <alanjhsu08@gmail.com>
2022-03-14 08:45:51 -0400

    measure vref

--- Firmware/DigitalFeedback/DigitalFeedback.ino
+++ Firmware/DigitalFeedback/DigitalFeedback.ino
@@ -26,6 +26,8 @@ int dacout;
 
 float VCC = 5;
 
+int tickcount;
+
 ADC0.SAMPCTRL = 2; // Decrease number of ADC clocks that the micro waits for. Default is 14. Should in theory reduce read time to 15us or so
 
 void setup() {
@@ -44,20 +46,34 @@ void receiveISR(int len) {
 }
 
 float measureVCC() {
+  VREF.CTRLA = VREF_ADC0REFSEL_1V1_gc;
+  ADC0.SAMPCTRL = 14;
+  ADC0.MUXPOS = ADC_MUXPOS_INTREF_gc;                  // Measure INTREF
+  ADC0.CTRLA = ADC_ENABLE_bm; 
   ADC0.COMMAND = ADC_STCONV_bm;                        // Start conversion
   while (ADC0.COMMAND & ADC_STCONV_bm);                // Wait for completion
   uint16_t adc_reading = ADC0.RES;                     // ADC conversion result
-  uint16_t voltage = 11264/adc_reading;
+  ADC0.SAMPCTRL = 2;
+  return 1126.4/adc_reading;
 }
 
 void loop() {
+  if (tickcount > 1000){
+    VCC = measureVCC();
+    tickcount = 0;
+  }
   v = analogRead(VIN); // Read in ADC
   i = analogRead(IIN); 
   
-  v = ( v * VCC / 4096 ) * 11
+  v = ( v * VCC / 4096 ) * 11 * scalev;
+  i = ( ( i * VCC / 4096 ) / 1.36 ) * scalei;
+
+  dacout = int(max(v, i))
   
   // Process int for transfer to DAC
-  dacout = (dacout & 0x3ff) << 2;
+  if (dacout > 1023)
+    dacout = 1023
+  dacout = dacout << 2;
   dacout |= (0b0001 << 12);
 
   PORTA.OUTCLR = (1 << CS); // CS Low
